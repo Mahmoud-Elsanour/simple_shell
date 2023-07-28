5cb2e2c8d3f4f815984206fa67ebc049f1221e4d
Abdallah Adel <abdallah.adel1221@gmail.com>
2023-07-29 02:31:29 +0300

    Create error_2.c

---
+++ error_2.c
@@ -0,0 +1,166 @@
+#include "shell.h"
+
+/**
+ * error_env - Creates an error message for shellby_env errors.
+ * @args: An array of arguments passed to the command.
+ *
+ * Return: The error string.
+ */
+char *error_env(char **args)
+{
+	char *err, *hist_str;
+	int len;
+
+	hist_str = _itoa(hist);
+	if (!hist_str)
+		return (NULL);
+
+	args--;
+	len = _strlen(name) + _strlen(hist_str) + _strlen(args[0]) + 45;
+	err = malloc(sizeof(char) * (len + 1));
+	if (!err)
+	{
+		free(hist_str);
+		return (NULL);
+	}
+
+	_strcpy(err, name);
+	_strcat(err, ": ");
+	_strcat(err, hist_str);
+	_strcat(err, ": ");
+	_strcat(err, args[0]);
+	_strcat(err, ": Unable to add/remove from environment\n");
+
+	free(hist_str);
+	return (err);
+}
+
+/**
+ * error_1 - Creates an error message for shellby_alias errors.
+ * @args: An array of arguments passed to the command.
+ *
+ * Return: The error string.
+ */
+char *error_1(char **args)
+{
+	char *err;
+	int len;
+
+	len = _strlen(name) + _strlen(args[0]) + 13;
+	err = malloc(sizeof(char) * (len + 1));
+	if (!err)
+		return (NULL);
+
+	_strcpy(err, "alias: ");
+	_strcat(err, args[0]);
+	_strcat(err, " not found\n");
+
+	return (err);
+}
+
+/**
+ * error_2_exit - Creates an error message for shellby_exit errors.
+ * @args: An array of arguments passed to the command.
+ *
+ * Return: The error string.
+ */
+char *error_2_exit(char **args)
+{
+	char *err, *hist_str;
+	int len;
+
+	hist_str = _itoa(hist);
+	if (!hist_str)
+		return (NULL);
+
+	len = _strlen(name) + _strlen(hist_str) + _strlen(args[0]) + 27;
+	err = malloc(sizeof(char) * (len + 1));
+	if (!err)
+	{
+		free(hist_str);
+		return (NULL);
+	}
+
+	_strcpy(err, name);
+	_strcat(err, ": ");
+	_strcat(err, hist_str);
+	_strcat(err, ": exit: Illegal number: ");
+	_strcat(err, args[0]);
+	_strcat(err, "\n");
+
+	free(hist_str);
+	return (err);
+}
+
+/**
+ * error_2_cd - Creates an error message for shellby_cd errors.
+ * @args: An array of arguments passed to the command.
+ *
+ * Return: The error string.
+ */
+char *error_2_cd(char **args)
+{
+	char *err, *hist_str;
+	int len;
+
+	hist_str = _itoa(hist);
+	if (!hist_str)
+		return (NULL);
+
+	if (args[0][0] == '-')
+		args[0][2] = '\0';
+	len = _strlen(name) + _strlen(hist_str) + _strlen(args[0]) + 24;
+	err = malloc(sizeof(char) * (len + 1));
+	if (!err)
+	{
+		free(hist_str);
+		return (NULL);
+	}
+
+	_strcpy(err, name);
+	_strcat(err, ": ");
+	_strcat(err, hist_str);
+	if (args[0][0] == '-')
+		_strcat(err, ": cd: Illegal option ");
+	else
+		_strcat(err, ": cd: can't cd to ");
+	_strcat(err, args[0]);
+	_strcat(err, "\n");
+
+	free(hist_str);
+	return (err);
+}
+
+/**
+ * error_2_syntax - Creates an error message for syntax errors.
+ * @args: An array of arguments passed to the command.
+ *
+ * Return: The error string.
+ */
+char *error_2_syntax(char **args)
+{
+	char *err, *hist_str;
+	int len;
+
+	hist_str = _itoa(hist);
+	if (!hist_str)
+		return (NULL);
+
+	len = _strlen(name) + _strlen(hist_str) + _strlen(args[0]) + 33;
+	err = malloc(sizeof(char) * (len + 1));
+	if (!err)
+	{
+		free(hist_str);
+		return (NULL);
+	}
+
+	_strcpy(err, name);
+	_strcat(err, ": ");
+	_strcat(err, hist_str);
+	_strcat(err, ": Syntax error: \"");
+	_strcat(err, args[0]);
+	_strcat(err, "\" unexpected\n");
+
+	free(hist_str);
+	return (err);
+}
